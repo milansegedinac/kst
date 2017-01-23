b1b4523a93c22d1dcf70f6cbac7c71353f8ba02a
Zoran Luledzija <zoran.luledzija@outlook.com>
2017-01-23 16:18:56 +0100

    Updated iita unit tests

--- kst/iita.py
+++ kst/iita.py
@@ -21,22 +21,26 @@ def iita(dataset, v):
     if (not isinstance(dataset, pd.DataFrame) and not isinstance(dataset, np.ndarray)) or (dataset.shape[1] == 1):
         sys.exit('data must be either a numeric matrix or a dataframe, with at least two columns.')
 
-    if sum(np.logical_not(np.logical_or(dataset == 0, dataset == 1)).sum()) != 0:
+    data = dataset
+    if isinstance(dataset, pd.DataFrame):
+        data = dataset.as_matrix()
+
+    if np.logical_not(np.logical_or(data == 0, data == 1)).sum() != 0:
         sys.exit('data must contain only 0 and 1')
 
     if v not in (1, 2, 3):
         sys.exit('IITA version must be specified')
 
     # inductively generated set of competing quasi orders
-    i = ind_gen(ob_counter(dataset))
+    i = ind_gen(ob_counter(data))
 
     # call chosen algorithm
     if v == 1:
-        ii = mini_iita(dataset, i)
+        ii = mini_iita(data, i)
     elif v == 2:
-        ii = corr_iita(dataset, i)
+        ii = corr_iita(data, i)
     elif v == 3:
-        ii = orig_iita(dataset, i)
+        ii = orig_iita(data, i)
 
     index = list(ii['diff.value']).index(min(ii['diff.value']))
     return {'diff': ii['diff.value'], 'implications': i[index], 'error.rate': ii['error.rate'][index], 'selection.set.index': index, 'v': v}
--- tests/test_iita.py
+++ tests/test_iita.py
@@ -1,12 +1,14 @@
 import unittest
 import pandas as pd
+import numpy as np
 from kst import iita
 
 
 class IitaTest(unittest.TestCase):
 
     def setUp(self):
-        self.dataset = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]})
+        self.dataframe = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]})
+        self.matrix = np.matrix([[1, 0, 0], [0, 1, 1], [1, 0, 1]])
 
     def test_iita_with_invalid_first_argument(self):
         self.assertRaises(SystemExit, lambda: iita(pd.DataFrame({'a': [1, 0, 1]}), v=1))
@@ -21,16 +23,16 @@ class IitaTest(unittest.TestCase):
         self.assertRaises(SystemExit, lambda: iita(dataset, v=1))
 
     def test_iita_with_invalid_second_argument(self):
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, -100))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, -1))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, 0))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, 4))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, 100))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, (1, 2)))
-        self.assertRaises(SystemExit, lambda: iita(self.dataset, [1, 2]))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, -100))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, -1))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, 0))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, 4))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, 100))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, (1, 2)))
+        self.assertRaises(SystemExit, lambda: iita(self.dataframe, [1, 2]))
 
-    def test_mini_iita(self):
-        response = iita(self.dataset, v=1)
+    def test_mini_iita_with_dataframe(self):
+        response = iita(self.dataframe, v=1)
 
         self.assertEqual([0.18518518518518515, 0.16666666666666666, 0.21296296296296294], response['diff'].tolist())
         self.assertEqual(0.5, response['error.rate'])
@@ -38,8 +40,8 @@ class IitaTest(unittest.TestCase):
         self.assertEqual(1, response['selection.set.index'])
         self.assertEqual(1, response['v'])
 
-    def test_corr_iita(self):
-        response = iita(self.dataset, v=2)
+    def test_corr_iita_with_dataframe(self):
+        response = iita(self.dataframe, v=2)
 
         self.assertEqual([0.18518518518518515, 0.16666666666666666, 0.21527777777777779], response['diff'].tolist())
         self.assertEqual(0.5, response['error.rate'])
@@ -47,8 +49,35 @@ class IitaTest(unittest.TestCase):
         self.assertEqual(1, response['selection.set.index'])
         self.assertEqual(2, response['v'])
 
-    def test_orig_iita(self):
-        response = iita(self.dataset, v=3)
+    def test_orig_iita_with_dataframe(self):
+        response = iita(self.dataframe, v=3)
+
+        self.assertEqual([0.20370370370370369, 0.39814814814814814, 0.21527777777777779], response['diff'].tolist())
+        self.assertEqual(0, response['error.rate'])
+        self.assertEqual([(2, 1)], response['implications'])
+        self.assertEqual(0, response['selection.set.index'])
+        self.assertEqual(3, response['v'])
+
+    def test_mini_iita_with_matrix(self):
+        response = iita(self.matrix, v=1)
+
+        self.assertEqual([0.18518518518518515, 0.16666666666666666, 0.21296296296296294], response['diff'].tolist())
+        self.assertEqual(0.5, response['error.rate'])
+        self.assertEqual([(0, 1), (0, 2), (2, 0), (2, 1)], response['implications'])
+        self.assertEqual(1, response['selection.set.index'])
+        self.assertEqual(1, response['v'])
+
+    def test_corr_iita_with_matrix(self):
+        response = iita(self.matrix, v=2)
+
+        self.assertEqual([0.18518518518518515, 0.16666666666666666, 0.21527777777777779], response['diff'].tolist())
+        self.assertEqual(0.5, response['error.rate'])
+        self.assertEqual([(0, 1), (0, 2), (2, 0), (2, 1)], response['implications'])
+        self.assertEqual(1, response['selection.set.index'])
+        self.assertEqual(2, response['v'])
+
+    def test_orig_iita_with_matrix(self):
+        response = iita(self.matrix, v=3)
 
         self.assertEqual([0.20370370370370369, 0.39814814814814814, 0.21527777777777779], response['diff'].tolist())
         self.assertEqual(0, response['error.rate'])
