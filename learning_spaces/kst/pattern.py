397aadeb02c67271e422d2a83bd8affdb7081a91
zarkoblagojevic00 <blagojevic.ra44.2018@uns.ac.rs>
2022-11-29 08:17:02 +0100

    implement pattern - computes the absolute frequencies of the response patterns

--- learning_spaces/kst/pattern.py
+++ learning_spaces/kst/pattern.py
@@ -1,8 +1,46 @@
+import sys
+from collections import Counter
+
 import numpy as np
 import pandas as pd
 
 
-def pattern(dataset, n = 5, p = None):
+def pattern(dataset, n=5, p=None):
+    """
+    pattern
+    computes the absolute frequencies of the response patterns,
+    and optionally, the absolute frequencies of a collection of
+    specified knowledge states in a dataset.
 
-    return {}
+    :param dataset: dataframe or matrix consisted of ones and zeros
+    :param n: number of patterns (must be greater than zero)
+    :param p: dataframe or matrix 
+    :return: dictionary representing pattern data
+    """
 
+    if n < 1:
+        sys.exit('Number of patterns must be greater than zero.')
+
+    data = dataset
+    if isinstance(dataset, pd.DataFrame):
+        data = dataset.values
+
+    def ks_to_str(ks): return ''.join((str(is_correct_answer) for is_correct_answer in ks))
+
+    pattern = Counter(np.apply_along_axis(ks_to_str, axis=1, arr=data))
+    if n > len(pattern):
+        n = len(pattern)
+
+    if p is None:
+        return {'response.patterns': pattern.most_common(n), 'states': p, 'n': n}
+
+    def getKnowledgeStatesFrequencies(p):
+        return np.apply_along_axis(lambda row: pattern[ks_to_str(row)], axis=1, arr=p)
+
+    if isinstance(p, pd.DataFrame):
+        states = p.assign(size=getKnowledgeStatesFrequencies(p.values))
+    else:
+        frequencies = getKnowledgeStatesFrequencies(p)
+        states = np.hstack((p, np.reshape(frequencies, (-1, 1))))
+
+    return {'response.patterns': pattern.most_common(n), 'states': states, 'n': n}
--- learning_spaces/kst/print_pat.py
+++ learning_spaces/kst/print_pat.py
@@ -7,6 +7,6 @@ def print_pat(x):
     """
     print('\nlargest response patterns in the data: {}'.format(x['n']))
     print(x['response.patterns'])
-    if(x['states'] != None):
+    if x['states'] is not None:
         print("Number of times a state occurs in the data:")
         print(x['states'])
